fb59c7299e1ebcda4c2998067013929fdfa3f542
Bearice Ren <bearice@gmail.com>
2012-02-27 16:19:44 +0800

    update with kumachan4js

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ var url = require('url');
 var http = require('http');
 var path = require('path');
 
-var Cookies = require('./libs/kumachan4js/cookies');
-var Session = require('./libs/kumachan4js/session');
-var mime    = require('./libs/kumachan4js/mime');
+var KumaServer = require('./libs/kumachan4js/server');
 var OAuth   = require('./libs/oauthjs/oauth');
 var config  = require('./config');
 
@@ -51,6 +49,7 @@ function logout(req,resp){
 
 function verify(req,resp){
     var oa = req.session.get("OAuth");
+    //console.info(oa);
     var token = req.info.query.oauth_verifier;
     if(oa){
         oa.setOAuthVerifier(token);
@@ -182,57 +181,6 @@ function commit(req,resp){
     });
 }
 
-function static(req,resp){
-    var p = path.normalize(req.info.pathname);
-    if(p=="/")p="/index.html";
-    if(!(/^\/static/.test(p))){
-        p = "/static" + p;
-    }
-    p = "."+p;
-    //console.info(p);
-    var stat = null;
-    try{
-    	stat = fs.statSync(p);
-    }catch(e){}
-    if(!stat || !stat.isFile()){
-        resp.writeHead(404);
-        resp.end("<html><body><h1>?_? Not found</h1></body></html>");
-        return;
-    }
-    var mtime = req.headers['if-modified-since'];
-    if(mtime){
-        if(mtime == stat.mtime.toString()){
-            resp.writeHead(304,{
-    	        'Date'          : stat.ctime.toString(),
-            	'Last-Modified' : stat.mtime.toString(),
-    	        'Cache-Control' : 'max-age=31536000',
-            });
-            resp.end();
-            return;
-        }
-    }
-    resp.writeHead(200,{
-    	'Content-Length': stat.size,
-    	'Content-Type'  : mime.mime_type(p),
-    	'Date'          : stat.ctime.toString(),
-    	'Last-Modified' : stat.mtime.toString(),
-    	'Cache-Control' : 'max-age=31536000',
-    });
-    if(req.head=='HEAD'){
-        resp.end();
-        return;
-    }
-    var s = fs.createReadStream(p);
-    s.on("end", function() {
-        resp.end();
-    });
-    s.on("error", function(e) {
-    	console.info(e);
-        resp.end();
-    });
-    s.pipe(resp);
-}
-
 var dispatch = {
     '/commit' : commit,
     '/login'  : login,
@@ -241,22 +189,8 @@ var dispatch = {
     '/check_session' : check_session,
 };
 
-http.createServer(function (request, response) {
-	console.info("%s %s %s",request.connection.remoteAddress,request.method,request.url);
-    request.cookies = new Cookies(request,response,config.cookies_key);
-    request.session = new Session(request.cookies,config.session.timeout,config.session.key);
-
-    request.info = url.parse(request.url,true);
-    response.setHeader("Server","KumaChan4J/1.0");
-    
-    var handler = dispatch[request.info.pathname];
-    if(handler){
-        handler(request,response);
-    }else{
-        static(request,response);
-    }
-}).listen(config.httpd.port,config.httpd.host);
-
+var server = KumaServer(dispatch,config.cookies_key,config.session);
+server.listen(config.httpd.port,config.httpd.host);
 
 console.log('Server running at %s:%d',config.httpd.host,config.httpd.port);
 
